4b611fe2d29bd6303fb0136ed23f016bc1b3989c
Thiago A. Lopes <thiagolopesbv@gmail.com>
2021-02-24 00:08:11 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  /*baseUrl: 'http://vacinas.thiagoalopes.dev.br',*/
+  /*baseUrl: '',*/
   baseUrl: 'http://localhost:8000',
-  clientId: '1',
-  secret: '0NPfkA18gfs2b2sxfiQKmInNzcnzvIgcdbhpFe6y',
+  clientId: '',
+  secret: '',
   tokenWhitelistedDomains: [ new RegExp('http://localhost:8000') ],
   tokenBlacklistedRoutes: [ new RegExp('\/oauth\/token')]
 
