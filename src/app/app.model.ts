802690191aad7086ac9bea0c2d0b44cc3a524af8
Thiago Alves Lopes <thiagolopesbv@gmail.com>
2021-03-03 15:34:21 -0400

    Adicionados cards para mais informações

--- src/app/app.model.ts
+++ src/app/app.model.ts
@@ -1,5 +1,25 @@
 export class AppModel {
 
+  estoque:any = [
+    {
+      id: 0,
+      doses_recebidas: 0,
+      doses_recebidas_1_dose: 0,
+      doses_recebidas_2_dose: 0,
+      doses_recebidas_indigenas: 0,
+      doses_recebidas_publico_geral: 0,
+      doses_distribuidas: 0,
+      doses_distribuidas_1_dose: 0,
+      doses_distribuidas_2_dose: 0,
+      doses_distribuidas_indigenas: 0,
+      doses_distribuidas_publico_geral: 0,
+      saldo_estoque: 0,
+      saldo_estoque_indegena: 0,
+      saldo_estoque_publico_geral: 0,
+      created_at: null,
+      updated_at: null
+    }
+  ];
   totalValcinados: string = '0';
   totalVacinadosHoje: string = '0';
   totalEstabelecimentos: string = '0';
@@ -9,7 +29,7 @@ export class AppModel {
   totalCategorias: string;
   totalSubcategorias: string;
   dataImportacao: string;
-  publicoAlvoFase = [{fase:'', total:0}];
+  publicoAlvoFase = [{fase: 0, total: 0}];
   totalAplicadasPorLaboratorio: [ 
     {
       vacina_fabricante_nome: '',
